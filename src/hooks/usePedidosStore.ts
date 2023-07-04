cdade56ccb6b37da5e0203d50d5cb2b85a45cc5a
guido morabito <guidomorabito161@hotmail.com>
2023-07-04 08:53:10 -0300

    dates

--- src/hooks/usePedidosStore.ts
+++ src/hooks/usePedidosStore.ts
@@ -1,6 +1,6 @@
-import { onAddPedido, onGetPedidos } from '@/store/slices/pedidosSlice'
+import { AppState, onAddPedido, onGetPedidos } from '@/store/slices/pedidosSlice'
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { collection, addDoc, DocumentData, Query, getDocs, orderBy, } from "firebase/firestore";
 import { db } from '@/firebase/firebase';
 import { Pedidos } from '@/interfaces/pedidos';
@@ -10,10 +10,13 @@ import { Pedidos } from '@/interfaces/pedidos';
 
 
 const usePedidosStore = () => {
+  const pedidosState = useSelector((state:AppState) => state.pedidos)
   const dispatch = useDispatch()
 
-  const startAddingPedidos = async (pedido: Pedidos) => {
+  
 
+  // subir los pedidos a firebase
+  const startAddingPedidos = async (pedido: Pedidos) => {
     const docRef = await addDoc(collection(db, "pedidos"), {
       ...pedido,
       fecha: new Date().toLocaleDateString()
@@ -21,14 +24,16 @@ const usePedidosStore = () => {
     dispatch(onAddPedido(pedido))
   }
 
+  // subir los pedidos al estado
   const startSettingPedidos = async () => {
     const querySnapshot = await getDocs(collection(db, "pedidos"));
     const pedidos: any = []
     querySnapshot.forEach((doc) => {
       pedidos.push({
         ...doc.data(),
-        fecha: new Date(Date.parse(doc.data().fecha)).toLocaleDateString()
       });
+
+      // Ordena los pedidos por fecha, de la mas actual a la mas vieja
       pedidos.sort((a:any, b:any) => {
         // Comparar las fechas en formato Date
         const dateA = new Date(a.fecha);
